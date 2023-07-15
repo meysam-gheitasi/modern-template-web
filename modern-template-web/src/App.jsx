2f0c42a4f3364f3a74499e1484d8c475d5ceb9ac
meysam1920 <121888134+meysam1920@users.noreply.github.com>
2023-07-15 18:21:10 +0330

    Creating main screen dev styles and navbar component holder devs in the App component

--- modern-template-web/src/App.jsx
+++ modern-template-web/src/App.jsx
@@ -1,8 +1,13 @@
 import Navbar from "./components/Navbar"
 
-
 const App = () => (
-  <Navbar />
+  <div className=" min-h-full bg-primary overflow-hidden">
+    <div className="flex justify-center items-center md:px-16 px-6">
+      <div className="w-full xl:min-w-[1280px]">
+        <Navbar />
+      </div>
+    </div>
+  </div>
 )
 
 export default App
