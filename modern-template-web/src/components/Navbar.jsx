d84c7456b547afc71cce93606127f58ae24729ac
meysam1920 <121888134+meysam1920@users.noreply.github.com>
2023-07-16 12:16:04 +0330

    Configure React Router Dam

--- modern-template-web/src/components/Navbar.jsx
+++ modern-template-web/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { logo, menu, close } from "../assets";
 import { navLink } from "../constants";
 
@@ -5,7 +6,14 @@ const Navbar = () => {
     return (
         <div className="flex justify-between items-center py-6 w-full">
             <img src={logo} alt="logo" className="w-[124px] h-[32px]" />
-            
+            <ul className=" hidden md:flex flex-1 list-none justify-end items-center">
+                {navLink.map((item, index) => (
+                    <li key={item.id} className={`cursor-pointer 
+                    ${index === navLink.length - 1 ? "mr-0" : "mr-4"}`}>
+                        <Link to={item.id}>{item.title}</Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
