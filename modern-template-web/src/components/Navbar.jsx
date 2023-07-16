ad8e8add043e54462d4e324ea09a35b176290b3f
meysam1920 <121888134+meysam1920@users.noreply.github.com>
2023-07-16 11:39:15 +0330

    Create logo in nav

--- modern-template-web/src/components/Navbar.jsx
+++ modern-template-web/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import { logo, menu, close } from "../assets"
+import { logo, menu, close } from "../assets";
+import { navLink } from "../constants";
 
 const Navbar = () => {
     return (
-        <div className="flex justify-between items  py-6 w-full">
-            Navbar
+        <div className="flex justify-between items-center py-6 w-full">
+            <img src={logo} alt="logo" className="w-[124px] h-[32px]" />
+           
         </div>
     );
 }
